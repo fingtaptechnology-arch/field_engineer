f66068c46641cfa4aa2043c3e073fdb622be78ff
Amna khan <kamna2755@gmail.com>
2026-07-24 18:47:20 +0500

    TEST

--- src/routes/amna.routes.jsx
+++ src/routes/amna.routes.jsx
@@ -1,7 +1,15 @@
 import About from '../pages/AboutUs'
+import LoginPage from '../pages/Login'
+import Privacy from '../pages/privacypolicy'
+
+import TermsConditions from '../pages/Terms&cnditions'
 
 const amnaRoutes = [
   { path: '/about-us', element: <About /> },
+  {path: '/login', element:<LoginPage/>},
+   {path: '/privacy', element: <Privacy/>},
+    {path: '/legal', element: <TermsConditions/>},
+
 ]
 
 export default amnaRoutes
