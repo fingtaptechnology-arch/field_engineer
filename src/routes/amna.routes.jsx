e67a04fcb32a0e2a8221d6ff2291ca400ef92076
Amna khan <kamna2755@gmail.com>
2026-07-27 15:37:22 +0500

    added Dashboard and Job-outlook

--- src/routes/amna.routes.jsx
+++ src/routes/amna.routes.jsx
@@ -1,14 +1,18 @@
 import About from '../pages/AboutUs'
 import LoginPage from '../pages/Login'
 import Privacy from '../pages/privacypolicy'
+import Dashboard from '../pages/Dashboard'
 
 import TermsConditions from '../pages/Terms&cnditions'
+import JobOutlook from '../pages/JobOutlook'
 
 const amnaRoutes = [
   { path: '/about-us', element: <About /> },
   {path: '/login', element:<LoginPage/>},
    {path: '/privacy', element: <Privacy/>},
     {path: '/legal', element: <TermsConditions/>},
+    {path: '/dashboard', element: <Dashboard/>},
+    {path: '/job-outlook', element: <JobOutlook/>},
 
 ]
 
