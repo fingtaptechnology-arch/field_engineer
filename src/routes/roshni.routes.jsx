2cbb0945fd5300abdad23d4f25d38e42e6122bb9
roshniqureshii14-lgtm <roshniqureshii14@gmail.com>
2026-07-29 15:57:56 +0500

    Final updates

--- src/routes/roshni.routes.jsx
+++ src/routes/roshni.routes.jsx
@@ -3,16 +3,16 @@ import HowItWorksBusiness from "../pages/HowItWorksBusiness"
 import HowItWorksEngineer from "../pages/HowItWorksEngineer"
 import Skills from "../pages/Skills"
 import Help from "../pages/Help"
-import ManageUsers from "../pages/ManageUsers"
 import TimeManagement from "../pages/TimeManagement"
+import ManageUsers from "../pages/UserManagement"
 
 const roshniRoutes = [
   { path: '/', element: <Home /> },
   { path: '/how-it-works-businesses', element: <HowItWorksBusiness /> },
-  { path: '/engineer', element: <HowItWorksEngineer /> },
+  { path: '/how-it-works-engineer', element: <HowItWorksEngineer /> },
   { path: '/skills', element: <Skills /> },
   { path: '/help', element: <Help /> },
-  { path: '/manage-users', element: <ManageUsers /> },
+  { path: '/user-management', element: <ManageUsers /> },
   { path: '/time-management', element: <TimeManagement /> },
 ]
 
